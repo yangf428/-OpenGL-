e4a0e2229145dead3499e9d959f0869b2a101586
yangf <1057936257@qq.com>
2019-05-02 20:37:33 +0800

    first commit

---
+++ 配置文件/FileCopy.bat
@@ -0,0 +1,10 @@
+
+copy glut.h "C:\Program Files (x86)\CodeBlocks\MinGW\include\GL" /y
+copy glut32.lib "C:\Program Files (x86)\CodeBlocks\MinGW\lib" /y
+copy OPENGL32.LIB "C:\Program Files (x86)\CodeBlocks\MinGW\lib" /y
+copy glut32.dll "C:\Windows\SysWOW64" /y
+
+copy GLAUX.h "C:\Program Files (x86)\CodeBlocks\MinGW\include\GL" /y
+copy glaux.dll "C:\Windows\SysWOW64" /y
+copy GLAUX.LIB "C:\Program Files (x86)\CodeBlocks\MinGW\lib" /y
+
